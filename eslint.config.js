39569e403db6506424a53138d497707b0561e95c
Yiryung <164003123+JungYiryung@users.noreply.github.com>
2024-11-01 01:16:36 +0900

    chore : ESLint 설정과 prettier 설정 (#2)
    * chore : 초기 폴더 구조 생성
    
    * chore : ESLint 설정과 prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      eslintPluginImport,
+    },
+    ignores: [
+      'node_modules/*',
+      '.prettierrc',
+      'eslint.config.js',
+      'package-lock.json',
+      'packaage.json',
+    ],
+  },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
