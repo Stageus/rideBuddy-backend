2a9b035470dca0ae2767769d82f7af74e10df2cb
jonny0323 <onlyjoke0323@gmail.com>
2024-11-17 19:24:46 +0900

    weather going

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
-import userRoute from './src/api/users/users.route';
-import infoRoute from './src/api/info/info.route';
-import weatherRoute from './src/api/weather/weather.route';
-import mypagesRoute from './src/api/mypages/mypages.route';
+// import userRoute from './src/api/users/users.tj/users.route.js';
+// import infoRoute from './src/api/info/info.route';
+import weatherRoute from './src/api/weather/weather.route.js';
+// import mypagesRoute from './src/api/mypages/mypages.route';
 //import cors from 'cors';
 /*global process */
 
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 
-app.use('/users', userRoute);
-app.use('/info', infoRoute);
+// app.use('/users', userRoute);
+// app.use('/info', infoRoute);
 app.use('/weather', weatherRoute);
-app.use('/mypages', mypagesRoute);
+// app.use('/mypages', mypagesRoute);
 
 app.listen(process.env.PORT, () => {
   console.log('5000포트에서 웹서버 실행중');
