315769c20c9c1641441b5cdfe37cbe144c6ae155
Yiryung <164003123+JungYiryung@users.noreply.github.com>
2024-11-19 22:40:37 +0900

    feat: /users/login/naver API와 /users/login/naver/callback API 추가 (#10)
    * feat : 로그인 테스트를 위한 html파일 추가
    
    * feat : /login/naver API와 /login/naver/callback API 완성

--- src/api/users/users.yr/users.service.js
+++ src/api/users/users.yr/users.service.js
@@ -1,33 +1,52 @@
-/* eslint-disable no-unused-vars */
 import axios from 'axios';
 import 'dotenv/config';
-import cookieParser from 'cookie-parser';
 import jwt from 'jsonwebtoken';
 
-/*global process :true*/
-/*eslint no-undef: "error"*/
-
 // 네이버 로그인 화면 띄우기
-export const userNaverLogin = async (req, res, next) => {
+export const userNaverLogin = (req, res, next) => {
   const NAVER_STATE = Math.random().toString(36).substring(2, 12);
 
-  res.redirect(
+  const loginWindow =
     `https://nid.naver.com/oauth2.0/authorize?` +
-      `response_type=code` +
-      `&client_id=${process.env.NAVER_CLIENT_ID}` +
-      `&state=${NAVER_STATE}` + //인코딩 해야할수도 테스트해보기
-      `&redirect_uri=${process.env.NAVER_CALLBACK_URL}`
-  );
+    `response_type=code` +
+    `&client_id=${process.env.NAVER_CLIENT_ID}` +
+    `&state=${NAVER_STATE}` + //인코딩 해야할수도 테스트해보기
+    `&redirect_uri=${process.env.NAVER_CALLBACK_URL}`;
+
+  res.send(loginWindow);
 };
 
 // 네이버 토큰발급 요청
 export const userNaverCallback = async (req, res, next) => {
-  const token = axios.get('');
-  // const token1;
+  const code = req.query.code;
+  const state = req.query.string;
+
+  const tokenUrl =
+    `https://nid.naver.com/oauth2.0/token?` +
+    `grant_type=authorization_code` +
+    `&client_id=${process.env.NAVER_CLIENT_ID}` +
+    `&client_secret=${process.env.NAVER_CLIENT_SECRET}` +
+    `&code=${code}` +
+    `&state=${state}`;
+
+  const response = await axios.get(tokenUrl);
+
+  if (response.status == 200) {
+    await axios.post('http://localhost:5000/users/login/naver/profile');
+  }
+
+  // res.set({
+  //   Content_type: 'text/plain',
+  //   refresh_token: `${response.data.refresh_token}`,
+  // });
+  // res.cookie('access_token', `${response.data.access_token}`);
+  // res.status(200).send();
 };
 
 // 네이버 액세스토큰으로 식별자 얻기
-export const userNaverProfile = async (req, res, next) => {};
+export const userNaverProfile = async (req, res, next) => {
+  console.log('가능?');
+};
 
 // 네이버 식별자 데이터 베이스에 저장 or 확인
 
