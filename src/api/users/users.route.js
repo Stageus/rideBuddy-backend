a4106f8cecb7b15f9ae751ea794b6f527c2688a1
JungYiryung <yi_ryung@naver.com>
2024-11-22 01:35:40 +0900

    remove: users.tj내에 있는 파일과 weather파일 내용, passport.js 내용 다시 작성하기 위해 삭제 , chore: Mysql 이 아니라 postgresql을 사용하기로 하여 파일 이름만 우선 변경함

--- src/api/users/users.route.js
+++ src/api/users/users.route.js
@@ -1,17 +1,6 @@
 import express from 'express';
-import {
-  googleAuth,
-  googleCallback,
-  register,
-  duplicate_id,
-  duplicate_tell,
-} from './users.tj/users.service.js';
-import {
-  userNaverLogin,
-  userNaverCallback,
-  userNaverProfile,
-  userDBCheck,
-} from './users.yr/users.service.js';
+import {} from './users.tj/users.service.js';
+import { userNaverLogin, userNaverCallback } from './users.yr/users.service.js';
 
 const router = express.Router();
 
@@ -19,21 +8,20 @@ const router = express.Router();
 router.post('/login/local');
 router.post('/login/naver', userNaverLogin);
 router.get('/login/naver/callback', userNaverCallback);
-router.get('/login/naver/profile', userNaverProfile);
-router.post('/login/oauth/user/check', userDBCheck);
-router.get('/fine-id');
-router.put('/fine-pw');
+router.get('/find-id');
+router.put('/find-pw');
+
 router.put('/tell');
 
 // Google OAuth 시작
-router.get('/google', googleAuth);
+router.get('/google');
 
 // Google OAuth 콜백 처리
-router.get('/google/callback', googleCallback);
+router.get('/google/callback');
 
-router.get('/duplicate-id', duplicate_id);
-router.get('/duplicate-tell', duplicate_tell);
-router.post('/register', register);
+router.get('/duplicate-id');
+router.get('/duplicate-tell');
+router.post('/register');
 router.post('/mail');
 router.get('/mail/check');
 router.delete('/my');
