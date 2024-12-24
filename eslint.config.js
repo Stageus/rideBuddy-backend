ce0a28f26c7bc090f46036703721bf96078fa87f
jonny0323 <onlyjoke0323@gmail.com>
2024-12-24 20:02:36 +0900

    initalize setting

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default [
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: { globals: globals.browser },
+    plugins: { eslintPluginImport },
+    ignores: [
+      'node_modules/*',
+      '.prettierrc',
+      'eslint.config.js',
+      'package-lock.json',
+      'package.json',
+    ],
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'no-useless-escape': 'off',
+    },
+  },
+];
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+// 가장 기본이 되는 express 시작
+
+import 'dotenv/config';
+// env 파일 활성화 config까지 해줌으로써 process.env 로 접근 가능하게 함.
+
+app.use(express.json());
+// josn 형식 -> js객체 형식으로 바꾼다.
+
+//============================================================================^기본 파일 설정^============================================
+
+import userRoute from './src/api/users/route.js';
+app.use('/users', userRoute);
+import infoRoute from './src/api/info/route.js';
+app.use('/users', infoRoute);
+import mypagesRoute from './src/api/mypages/route.js';
+app.use('/users', mypagesRoute);
+import weatherRoute from './src/api/weather/route.js';
+app.use('/users', weatherRoute);
+
+//============================================================================^기본 라우팅 설정^==========================================
+
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.PORT}포트에서 웹서버 실행중`);
+});
