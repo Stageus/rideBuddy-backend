38a8a1555cf5537d916d4d53581d52db3cd3c622
JungYiryung <yi_ryung@naver.com>
2024-12-30 16:41:10 +0900

    feat: verifyMailToken.js 완성

--- src/api/users/route.js
+++ src/api/users/route.js
@@ -26,7 +26,7 @@ router.get('/login/naver/callback', naverCreateToken);
 router.get('/google', userGoogleLogin); //완료
 //router.get('/google/callback', userGoogleCallback, createToken); //createToken 없앴음
 router.get('/find-id', validateRegx);
-router.put('/change-pw', validateRegx); //db에 True가 되어있어야함 checkMailStatus, changePw
+router.put('/change-pw', validateRegx, checkMailStatus, changePw); //db에 True가 되어있어야함
 router.put('/change-pw/mypages', verifyLoginToken, validateRegx, changePw);
 router.get('/duplicate-id', duplicateId);
 //메일 중복체크 라우터 추가하기 - 태준
