0ffd7eb659d9e6768d34dd590c54bec9c53646e5
Yiryung <164003123+JungYiryung@users.noreply.github.com>
2025-01-02 18:03:26 +0900

    feat: verifymailToken (#25)
    * initalize setting
    
    * setting complete
    
    * complete setting
    
    * finish setting
    
    * refactor: 구조 재설정 후 코드 옮김
    
    * chore: 실수로 지운거 원상복구
    
    * chore: 삭제된 주석 복구
    
    * refactor: 구조 전체적으로 수정
    
    * fix: 이상하게 예전 커밋들과 합쳐지는데 충돌해결을 잘못한것 같다.. 필요없는 파일 다 지움
    
    * fix : 서버 실행되도록 수정
    
    * feat : 미들웨어 폴더 생성 및 route.js 수정
    
    * feat : 폴더별로 함수 구분해서 다시  넣어줌
    
    * fix : 서버 돌아가게 수정
    
    * fix : 서버 돌아가게 수정
    
    * chore: verifyToken 미들웨로 토큰검증관련 함수 모두 모음
    
    * feat : verifyToken.js 완료
    
    * feat: route에 미들웨어 추가 및 genIdentToken 추가
    
    * feat: mail API 위해 필요한 미들웨어와 service로직 정함
    
    * feat: /register api 미들웨어 추가
    
    * feat : validateRegx.js 완성
    
    * fix: postman 에러 나던거 해결
    
    * feat: verifyMailToken.js 완성
    
    * chore: 주석 추가
    
    ---------
    
    Co-authored-by: jonny0323 <onlyjoke0323@gmail.com>

--- src/api/users/middleware/verifyMailToken.js
+++ src/api/users/middleware/verifyMailToken.js
@@ -6,44 +6,45 @@ import jwt from 'jsonwebtoken';
 
 //jwt를 검증하는 로직 , 밑에 함수에서 필요
 const verifyJWT = (token) => {
-  const secretKey = process.env.JWT_IDENT_SECRET;
-  // db와 통신해서 토큰이 일치하는지 알아봐야하므로
-  // 1. 토큰 verify 해서 mail 빼내고
-  // 2.
+  const secretKey = process.env.JWT_MAIL_SECRET;
 
-  // 5분있다가 어짜피 idenToken 만료되는데 굳이 생성시간을 넣어야하나?
-  //
+  let result = {
+    errName: '',
+    decoded: '',
+  };
 
-  //   let result = {
-  //     errName: '',
-  //     decoded: '',
-  //   };
-
-  //   jwt.verify(token, secretKey, function (err, decoded) {
-  //     if (err) {
-  //       result.errName = err.name;
-  //       result.decoded = decoded; //undefined
-  //       result.err = err;
-  //     } else {
-  //       result.errName = null;
-  //       result.decoded = decoded;
-  //     }
-  //   });
-  //   return result;
+  jwt.verify(token, secretKey, function (err, decoded) {
+    if (err) {
+      result.errName = err.name;
+      result.decoded = decoded; //undefined
+      result.err = err;
+    } else {
+      result.errName = null;
+      result.decoded = decoded;
+    }
+  });
+  return result;
 };
 
 // 토큰이 유효한지 체크
 export const verifyMailToken = async (req, res, next) => {
-  const identToken = req.body['identification_token'];
+  // wrapper 필요 mail_token으로 안올시 에러남
+  const mailToken = req.body['mail_token'];
 
-  const identResult = verifyJWT(identToken);
+  const mailResult = verifyJWT(mailToken);
 
   //토큰 만료가 아닌 다른에러라면
   const errorName = ['JsonWebTokenError', 'NotBeforeError'];
   for (const error of errorName) {
-    if (accessResult.errNmae === error) next(accessResult.err);
-    if (refreshResult.errName === error) next(refreshResult.err);
+    if (mailResult.errName === error) next(mailResult.err);
   }
 
-  // 1. db와 통신하여 메일
+  //1. 만료되었다면 만료되었다고
+  if (mailResult.errName == 'TokenExpiredError') {
+    next(new Error('토큰이 만료되었음.'));
+  }
+  //2. 검증성공시
+  else {
+    next();
+  }
 };
