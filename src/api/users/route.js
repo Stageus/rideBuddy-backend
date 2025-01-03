f67fce49dfac57bb9e8acb13f6ab70efb8e90e3f
jonny0323 <onlyjoke0323@gmail.com>
2025-01-03 16:34:14 +0900

    continue making

--- src/api/users/route.js
+++ src/api/users/route.js
@@ -6,7 +6,9 @@ import {
   register,
   duplicateId,
   mailSendregister,
-  duplicateMail
+  duplicateMail,
+  mailSendChanePw,
+  mailCheck,
 } from './tj/service.js';
 
 import {
@@ -24,21 +26,61 @@ import { checkMailStatus } from './middleware/checkMailStatus.js';
 import wrapController from '#utility/wrapper.js';
 const router = express.Router();
 
-router.post('/login/local', wrapController(validateRegx), wrapController(localCreateToken)); //localCreateToken
+router.post(
+  '/login/local',
+  wrapController(validateRegx),
+  wrapController(localCreateToken)
+); //localCreateToken
 router.post('/login/naver', wrapController(naverLogin));
 router.get('/login/naver/callback', wrapController(naverCreateToken));
 router.get('/login/google', wrapController(userGoogleLogin)); //완료
 router.get('/google/callback', wrapController(googleCreateToken)); //createToken 없앴음
 router.get('/find-id', wrapController(validateRegx));
 router.put('/change-pw', wrapController(validateRegx)); //db에 True가 되어있어야함 checkMailStatus, changePw
-router.put('/change-pw/mypages', wrapController(verifyLoginToken), wrapController(validateRegx), wrapController(changePw));
-router.get('/duplicate-id', wrapController(validateRegx), wrapController(duplicateId));
-router.get('/duplicate-mail', wrapController(validateRegx), wrapController(duplicateMail));
-router.post('/register', wrapController(validateRegx), wrapController(duplicateId) , wrapController(duplicateMail), wrapController(checkMailStatus), wrapController(register));
-router.post('/mail', wrapController(validateRegx), wrapController(mailSendregister)); //code랑 mailToken 생성해서 db에 저장
-router.post('/mail/withId', validateRegx); //code랑 mailToken 생성해서 db에 저장
-router.get('/mail/check', verifyMailToken, validateRegx); // db에 저장한거 True로 수정
-router.delete('/my', verifyLoginToken, deleteuser);
+router.put(
+  '/change-pw/mypages',
+  wrapController(verifyLoginToken),
+  wrapController(validateRegx),
+  wrapController(changePw)
+);
+router.get(
+  '/duplicate-id',
+  wrapController(validateRegx),
+  wrapController(duplicateId)
+);
+router.get(
+  '/duplicate-mail',
+  wrapController(validateRegx),
+  wrapController(duplicateMail)
+);
+router.post(
+  '/register',
+  wrapController(validateRegx),
+  wrapController(duplicateId),
+  wrapController(duplicateMail),
+  wrapController(checkMailStatus),
+  wrapController(register)
+);
+router.post(
+  '/mail',
+  wrapController(validateRegx),
+  wrapController(mailSendregister)
+); //code랑 mailToken 생성해서 db에 저장
+router.post(
+  '/mail/withId',
+  wrapController(validateRegx),
+  wrapController(mailSendChanePw)
+); //code랑 mailToken 생성해서 db에 저장
+router.get(
+  '/mail/check',
+  wrapController(verifyMailToken),
+  wrapController(validateRegx),
+  wrapController(mailCheck)
+); // db에 저장한거 True로 수정
+router.delete(
+  '/my',
+  wrapController(verifyLoginToken),
+  wrapControllerd(deleteuser)
+);
 
 export default router;
-
