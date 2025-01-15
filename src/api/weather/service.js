b40cb091500d3e5622ffc9d43717fdc35570591e
JungYiryung <yi_ryung@naver.com>
2025-01-15 15:47:50 +0900

    feat: 미세먼지부분 일부 완성

--- src/api/weather/service.js
+++ src/api/weather/service.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { getWeather } from './repository.js';
-
 import 'dotenv/config';
 import proj4 from 'proj4';
+import wrap from '#utility/wrapper.js';
 
-const weather = async (req, res) => {
-  const nx = req.body.nx; // 위도 37 어쩌구
+const weather = wrap(async (req, res) => {
   const ny = req.body.ny; // 경도 126 어쩌구
+  const nx = req.body.nx; // 위도 37 어쩌구
 
-  // 날씨 부분
+  // 법정동 불러오기
   const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?
                 request=coordsToaddr&coords=${ny},${nx}&sourcecrs=epsg:4326&orders=admcode,legalcode,addr,roadaddr&output=JSON`;
   const response = await axios({
@@ -19,6 +19,13 @@ const weather = async (req, res) => {
       'x-ncp-apigw-api-key': process.env.MAP_API_KEY
     }
   });
+
+  // 미세먼지용 법정동
+  const legalSido = response.data.results[1].region.area1.name;
+  const legalDong = response.data.results[1].region.area3.name;
+
+  // results[0]은 행정동이고 results[1]은 법정동이라서 내거는 법정동으로 했어
+  // 태준이 너도 고민해보고 맞는걸로 해
 
   // const results = response.data.results;
   // console.log(results[0].region.area1.name);
@@ -51,16 +58,44 @@ const weather = async (req, res) => {
   // };
 
   // 미세먼지 부분
-  // 1. nx ny를 grs80으로 변경
-  //
-  proj4.defs(
-    'EPSG:5186',
-    '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=600000 +ellps=GRS80 +units=m +no_defs'
-  );
-  const result = proj4('EPSG:5186', [ny, nx]);
+  const encodingServiceKey = process.env.PUBLIC_SERVICE_KEY;
+  const decodingServiceKey = decodeURIComponent(`${encodingServiceKey}`);
 
-  console.log('결과', result);
+  //1. fetch로 사용자 위치(법정동)에 대한 TM 기준좌표 조회
+  const TMurl = 'http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getTMStdrCrdnt';
+  const TMParams = {
+    serviceKey: decodingServiceKey,
+    umdName: legalDong,
+    returnType: 'json'
+  };
+  const TMQuery = new URLSearchParams(TMParams).toString();
+  let tmX, tmY;
+  const TMFetch = await fetch(`${TMurl}?${TMQuery}`);
+  const TMResult = await TMFetch.json();
+  // 동이름이 같은 지역이 있는경우 배열로 나오기때문에 '시도'이름으로 필터링하여 TM좌표 추출
+  for (let array of TMResult.response.body.items) {
+    if (array.sidoName == legalSido) {
+      tmX = array.tmX;
+      tmY = array.tmY;
+    }
+  }
+
+  //2. TM좌표 기준 가장 근접한 측정소 조회
+  const nearStaionUrl = `http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList`;
+  const nearStationParams = {
+    serviceKey: decodingServiceKey,
+    returnType: 'json',
+    tmX: tmX,
+    tmY: tmY
+  };
+  const nearStationQuery = new URLSearchParams(nearStationParams).toString();
+  const nearStationFetch = await fetch(`${nearStaionUrl}?${nearStationQuery}`);
+  const nearStationResult = await nearStationFetch.json();
+  const stationName = nearStationResult.response.body.items[0].stationName;
+
+  //3. 측정소에 해당하는 값 불러오기
+  // 여기서부터는 Pm2 필요
   res.status(200).send({});
-};
+});
 
 export default weather;
