6f5ba7d18885eae21f872f6bef6673f64d19ca68
JungYiryung <yi_ryung@naver.com>
2025-03-16 15:02:50 +0900

    Test: wehater 데이터 안들어간 오류

--- backGroundGetData/index.js
+++ backGroundGetData/index.js
@@ -33,7 +33,8 @@ cron.schedule(`5 0 * * *`, async () => {
   deleteWeatherData(23);
 });
 // 2시간에 한번씩 getAirData 호출
-cron.schedule('0 20 */2 * * *', async () => {
+// cron.schedule('0 20 */2 * * *', async () => {
+cron.schedule('0 3 * * * *', async () => {
   try {
     const currentTime = new Date().toString();
     console.log(`주기적으로 미세먼지 함수 실행중, 현재시각 ${currentTime}`);
