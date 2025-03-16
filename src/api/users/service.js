954156957cc6d5dee1d69876ac21a7bc9de2d6b7
Yiryung <164003123+JungYiryung@users.noreply.github.com>
2025-03-16 14:26:24 +0900

    Fix (#85)
    * Update README.md
    
    * Update README.md
    
    * chore: log 400이상만 찍히도록 수정
    
    * chore: 테스트용 코드 삭제
    
    * feat: pm2실행을 위한 script 명령어 추가
    
    * chore: 서버실행중 확인을 위한 주석추가
    
    * fix: 리눅스 설정으로 인한 80번 포트 허용 위해 sudo 추가
    
    * fix: setcap 설정으로 인해 sudo 코드 삭제
    
    * fix: setcap 설정으로 인해 sudo 코드 삭제
    
    * fix: npm run으로 인한 env인식문제 해결
    
    * fix: cors 에러 위한 cors 추가
    
    * refactor: get에 body 못넣는 이슈로 post로 변경
    
    * fix: 코드 수정후 데이터 빈값들어가던거 수정
    
    * feat: info/roads , info/ceenters 에서 위경도 값도 보내주는걸로 수정
    
    * chore: search 정규표현식을 공백도 허용하도록 수정
    
    * fix: /info 결과값으로 좋아요와 pointidx와 idx를 구분하도록 고치고, /info/center에서 결과값으로 좋아요 주도록 고치고, /info/roads 에서 좋아요 주고 roadidx도 추가로 주도록 수정함
    
    * /info/pin의 결과도  /info의 결과와 비슷하게 오도록 수정함
    
    * chore: 에러 해결을 위한 주석 추가
    
    * chore: 에러 해결을 위한 주석 추가
    
    * fix: /roads/:roadPointIdx 부분 잘못 쿼리짠거 코드 수정
    
    * Test: naver OAuth로그인을 위한 테스트 코드
    
    * Fix: 프론트에서 code와 status 받는것으로 로직 변경
    
    * Fix : 테스트용 코드로 인해 서버 멈춤문제와 변수 잘못 넣은 부분 수정
    
    * Test: oauth test위한 콘솔 코드 추가
    
    * Fix: state에 encoding 추가

--- src/api/users/service.js
+++ src/api/users/service.js
@@ -222,7 +222,7 @@ export const naverLogin = wrap((req, res) => {
 // 네이버 토큰발급 요청후 로직거쳐 localToken 발급
 export const naverCreateToken = wrap(async (req, res) => {
   const { code, state } = req.body;
-  // const encodedState = encodeURI(state);
+  const encodedState = encodeURI(state);
 
   const tokenUrl =
     `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code` +
