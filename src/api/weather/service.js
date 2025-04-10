08763a090396aa7b7a8f64a508cf21498ca51756
JungYiryung <yi_ryung@naver.com>
2025-04-11 00:17:24 +0900

    Chore: 필요없는 코드 주석처리

--- src/api/weather/service.js
+++ src/api/weather/service.js
@@ -64,8 +64,6 @@ const weather = wrap(async (req, res) => {
     params: TMParams
   });
 
-  console.log('TMaxios 콘솔1', TMaxios.data.response.body);
-
   if (!TMaxios.data.response.body) {
     for (let i = 0; i < 10; i++) {
       TMaxios = await axios({
@@ -127,7 +125,7 @@ const weather = wrap(async (req, res) => {
     }
   }
 
-  console.log('TMaxios 콘솔2', TMaxios.data.response.body);
+  // console.log('TMaxios 콘솔2', TMaxios.data.response.body);
 
   console.log('tmx', tmX);
   console.log('tmy', tmY);
