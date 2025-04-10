e4cfe3d0644079fcc39d8ef4b4ba37efa75fee83
TaeJoon Lee <taejoonlee@TaeJoonui-noteubug.local>
2025-04-10 15:12:27 +0900

    cdd

--- src/api/users/service.js
+++ src/api/users/service.js
@@ -35,7 +35,7 @@ import sendMailUtil from './utility/mail.js';
 
 //구글 oauth
 export const userGoogleLogin = wrap((req, res) => {
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.GOOGLE_REDIRECT_URL)}&response_type=code&scope=email profile`;
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
   console.log("redirect_uri: ", process.env.GOOGLE_REDIRECT_URL);
   console.log("client_id: ", process.env.GOOGLE_CLIENT_ID);
   console.log("code: ", code);
